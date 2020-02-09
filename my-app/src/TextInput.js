50cfc824f76d4407751d44456995077f930443a2
Hannah Wlasowicz <hannahwl@buffalo.edu>
2020-02-09 00:06:47 -0500

    Adding sqlite

--- my-app/src/TextInput.js
+++ my-app/src/TextInput.js
@@ -1,4 +1,7 @@
 import React, {Component} from 'react';
+import { openDatabase } from 'react-native-sqlite-storage';
+
+var db = openDatabase({ name: 'UserDatabase.db' });
 
 class TextInput extends Component {
 
@@ -13,6 +16,23 @@ class TextInput extends Component {
       event.preventDefault()
       const data = this.state
       console.log("Final data is",  data)
+
+      db.transaction(function(txn) {
+     txn.executeSql(
+       "SELECT name FROM sqlite_master WHERE type='table' AND name='table_user'",
+       [],
+       function(tx, res) {
+         console.log('item:', res.rows.length);
+         if (res.rows.length == 0) {
+           txn.executeSql('DROP TABLE IF EXISTS table_user', []);
+           txn.executeSql(
+             'CREATE TABLE IF NOT EXISTS table_user(user_id INTEGER PRIMARY KEY AUTOINCREMENT, user_name VARCHAR(20), user_contact INT(10), user_address VARCHAR(255))',
+             []
+           );
+         }
+       }
+     );
+   });
     }
 
     handleInputChange = (event) => {
