9679f17546c822c3518b1c3d3ff5d20b9dbd4864
Hannah Wlasowicz <hannahwl@buffalo.edu>
2020-02-09 00:10:18 -0500

    added insert message into the database

--- my-app/src/TextInput.js
+++ my-app/src/TextInput.js
@@ -19,14 +19,20 @@ class TextInput extends Component {
 
       db.transaction(function(txn) {
      txn.executeSql(
-       "SELECT name FROM sqlite_master WHERE type='table' AND name='table_user'",
+       "SELECT name FROM sqlite_master WHERE type='table' AND name='table_messages'",
        [],
        function(tx, res) {
          console.log('item:', res.rows.length);
          if (res.rows.length == 0) {
-           txn.executeSql('DROP TABLE IF EXISTS table_user', []);
+           txn.executeSql('DROP TABLE IF EXISTS table_messages', []);
            txn.executeSql(
-             'CREATE TABLE IF NOT EXISTS table_user(user_id INTEGER PRIMARY KEY AUTOINCREMENT, user_name VARCHAR(20), user_contact INT(10), user_address VARCHAR(255))',
+             'CREATE TABLE IF NOT EXISTS table_messages(data VARCHAR(255))',
+             []
+           );
+         }
+         else{
+           txn.executeSql(
+             'INSERT INTO TABLE table_messages(data VARCHAR(255))',
              []
            );
          }
